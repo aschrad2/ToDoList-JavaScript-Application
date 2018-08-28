6103d606ff477626ca6da2ee169a6ab2f721d82e
Austin Schrader <austin.jschrader@gmail.com>
2018-08-28 01:36:22 -0700

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,13 @@
+var addButton = document.getElementById("add");
+var taskInput = document.getElementById("task");
+var taskList = document.getElementById("taskList");
+
+
+addButton.addEventListener("click", function(){
+    var task = taskInput.value;
+    if(task.trim()){
+       var newItem = document.createElement("LI");
+       var taskText = document.createTextNode(task);
+       newItem.appendChild(taskText);
+    }
+})
